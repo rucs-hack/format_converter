d07abee7130b5b7e066300d209d08db8fc65f31f
Carey PridgeonE <carey.pridgeon@gmail.com>
2023-02-18 15:23:41 +0000

    update

--- src/fconv.cpp
+++ src/fconv.cpp
@@ -1,6 +1,6 @@
 /**
 * Author      : Dr Carey Pridgeon
-* Copyright   : Dr Carey Pridgeon 2009
+* Copyright   : Dr Carey Pridgeon 2023
 * Licence     : Licensed under the Apache License, Version 2.0 (the "License");
 *             : you may not use this file except in compliance with the License.
 *             : You may obtain a copy of the License at
